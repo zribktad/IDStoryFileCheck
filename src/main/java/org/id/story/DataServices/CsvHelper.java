791d20aef55ef673454d2222650282d8c46c3f01
zribktad <zribktad@fel.cvut.cz>
2024-02-24 23:27:00 +0100

    Create csv iterator

---
+++ src/main/java/org/id/story/DataServices/CsvHelper.java
@@ -0,0 +1,41 @@
+package org.id.story.DataServices;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CsvHelper {
+
+    static Map<String, Integer> getColumnIndexesFromLine(String headerLine) {
+        String[] columns = headerLine.split(",");
+        Map<String, Integer> columnIndexes = new HashMap<>();
+        for (int i = 0; i < columns.length; i++) {
+            columnIndexes.put(columns[i], i);
+        }
+        return columnIndexes;
+    }
+
+
+    static Map<String, String> getLineInMap(String currentLine, Map<String, Integer> columnIndexes) throws RuntimeException {
+
+        String[] values = currentLine.split(",");
+        Map<String, String> row = new HashMap<>();
+        for (Map.Entry<String, Integer> entry : columnIndexes.entrySet()) {
+            String columnName = entry.getKey();
+            Integer index = entry.getValue();
+            if (index < values.length) {
+                row.put(columnName, values[index]);
+            } else {
+                throw new RuntimeException("Not equal number of elements in line: " + currentLine);
+            }
+        }
+
+        return row;
+    }
+
+    static String getPathFromResources(String filePath) {
+        return CsvHelper.class.getClassLoader().getResource(filePath).getPath();
+    }
+}
+

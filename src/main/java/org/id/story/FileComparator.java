ad02614b21b1d361ff767de5b1d1b5840a67246b
zribktad <zribktad@fel.cvut.cz>
2024-02-24 22:40:26 +0100

    Interval loop with stop command

--- src/main/java/org/id/story/FileComparator.java
+++ src/main/java/org/id/story/FileComparator.java
@@ -2,10 +2,39 @@ package org.id.story;
 
 import org.id.story.Controller.FileComparatorController;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class FileComparator {
     public static void main(String[] args) {
 
-        FileComparatorController controller = new FileComparatorController();
-        controller.startComparison();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String userInput;
+
+        long interval = 1000;
+
+        try {
+            while (true) {
+                //Code
+                System.out.println("Comparing files...");
+                if (reader.ready()) {
+                    userInput = reader.readLine();
+                    if (userInput.equalsIgnoreCase("stop")) {
+                        break;
+                    }
+                }
+                Thread.sleep(interval);
+            }
+        } catch (InterruptedException | IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
